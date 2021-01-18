bd501414b592b2aa5ae4d2be6dbdaa9fc5dd6d55
Frank Escobar <fescobar.systems@gmail.com>
2021-01-18 13:06:50 +0000

    #136-clean_results doesnt work with a large amount of results

--- allure-docker-scripts/cleanAllureResults.sh
+++ allure-docker-scripts/cleanAllureResults.sh
@@ -5,7 +5,7 @@ PROJECT_ID=$1
 echo "Cleaning results for PROJECT_ID: $PROJECT_ID"
 PROJECT_RESULTS_DIRECTORY=$STATIC_CONTENT_PROJECTS/$PROJECT_ID/results
 if [ "$(ls -A $PROJECT_RESULTS_DIRECTORY | wc -l)" != "0" ]; then
-    ls -d $PROJECT_RESULTS_DIRECTORY/* | grep -v history | xargs rm 2> /dev/null
+    find $PROJECT_RESULTS_DIRECTORY/ -maxdepth 1 -type f -print0 | xargs -0 rm 2> /dev/null
 fi
 
 echo "Results cleaned for PROJECT_ID: $PROJECT_ID"
