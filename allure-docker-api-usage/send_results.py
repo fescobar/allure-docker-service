b28dca2b689afa33f46b2ce1b18007725b6711a1
Adrian Gonciarz <adrian.gonciarz@kitopi.com>
2021-07-02 17:29:35 +0200

    feat: Refactor Python scripts to be more modular

--- allure-docker-api-usage/send_results.py
+++ allure-docker-api-usage/send_results.py
@@ -1,4 +1,5 @@
 import os, requests, json, base64
+from requests import Response
 
 # This directory is where you have all your results locally, generally named as `allure-results`
 allure_results_directory = '/allure-results-example'
@@ -6,70 +7,88 @@ allure_results_directory = '/allure-results-example'
 allure_server = 'http://localhost:5050'
 # Project ID according to existent projects in your Allure container - Check endpoint for project creation >> `[POST]/projects`
 project_id = 'default'
-#project_id = 'my-project-id'
+# project_id = 'my-project-id'
 
+# Connection configuration
+send_results_uri = f'{allure_server}/allure-docker-service/send-results'
+generate_report_uri = f'{allure_server}/allure-docker-service/generate-report'
+ssl_verification = True
 
 current_directory = os.path.dirname(os.path.realpath(__file__))
 results_directory = current_directory + allure_results_directory
 print('RESULTS DIRECTORY PATH: ' + results_directory)
 
-files = os.listdir(results_directory)
 
-print('FILES:')
-results = []
-for file in files:
+def get_file_as_result(filename):
+    file_path = results_directory + "/" + filename
     result = {}
 
-    file_path = results_directory + "/" + file
-    print(file_path)
-
     if os.path.isfile(file_path):
-        try:
-            with open(file_path, "rb") as f:
-                content = f.read()
-                if content.strip():
-                    b64_content = base64.b64encode(content)
-                    result['file_name'] = file
-                    result['content_base64'] = b64_content.decode('UTF-8')
-                    results.append(result)
-                else:
-                    print('Empty File skipped: '+ file_path)
-        finally :
-            f.close()
+        with open(file_path, "rb") as f:
+            content = f.read()
+            if content.strip():
+                b64_content = base64.b64encode(content)
+                result['file_name'] = filename
+                result['content_base64'] = b64_content.decode('UTF-8')
+                print(f'Successfully encoded result for {file_path}')
+                return result
+            else:
+                print('Empty File skipped: ' + file_path)
     else:
-        print('Directory skipped: '+ file_path)
+        print('Directory skipped: ' + file_path)
 
-headers = {'Content-type': 'application/json'}
-request_body = {
-    "results": results
-}
-json_request_body = json.dumps(request_body)
 
-ssl_verification = True
+def gather_result_files():
+    files = os.listdir(results_directory)
+    print('FILES:')
+    results = list(filter(lambda f: f is not None, [get_file_as_result(file) for file in files]))
+    return results
 
-print("------------------SEND-RESULTS------------------")
-response = requests.post(allure_server + '/allure-docker-service/send-results?project_id=' + project_id, headers=headers, data=json_request_body, verify=ssl_verification)
-print("STATUS CODE:")
-print(response.status_code)
-print("RESPONSE:")
-json_response_body = json.loads(response.content)
-json_prettier_response_body = json.dumps(json_response_body, indent=4, sort_keys=True)
-print(json_prettier_response_body)
 
-# If you want to generate reports on demand use the endpoint `GET /generate-report` and disable the Automatic Execution >> `CHECK_RESULTS_EVERY_SECONDS: NONE`
-"""
-print("------------------GENERATE-REPORT------------------")
-execution_name = 'execution from my script'
-execution_from = 'http://google.com'
-execution_type = 'teamcity'
-response = requests.get(allure_server + '/allure-docker-service/generate-report?project_id=' + project_id + '&execution_name=' + execution_name + '&execution_from=' + execution_from + '&execution_type=' + execution_type, headers=headers, verify=ssl_verification)
-print("STATUS CODE:")
-print(response.status_code)
-print("RESPONSE:")
-json_response_body = json.loads(response.content)
-json_prettier_response_body = json.dumps(json_response_body, indent=4, sort_keys=True)
-print(json_prettier_response_body)
+def log_response(response: Response, with_report_url=False):
+    print("STATUS CODE:")
+    print(response.status_code)
+    print("RESPONSE:")
+    json_prettier_response_body = json.dumps(response.json(), indent=4, sort_keys=True)
+    print(json_prettier_response_body)
+    if with_report_url:
+        print('ALLURE REPORT URL:')
+        print(response.json()['data']['report_url'])
 
-print('ALLURE REPORT URL:')
-print(json_response_body['data']['report_url'])
-"""
+
+def send_results_to_allure_server():
+    print("------------------SEND-RESULTS------------------")
+    request_body = {
+        "results": gather_result_files()
+    }
+    uri_params = {
+        'project_id': project_id
+    }
+    response = requests.post(send_results_uri, params=uri_params, json=request_body, verify=ssl_verification)
+    log_response(response)
+
+
+def generate_allure_report():
+    """
+    If you want to generate reports on demand use the endpoint `GET /generate-report` and disable the Automatic Execution >> `CHECK_RESULTS_EVERY_SECONDS: NONE`
+    """
+    print("------------------GENERATE-REPORT------------------")
+    execution_name = 'execution from my script'
+    execution_from = 'http://google.com'
+    execution_type = 'teamcity'
+
+    uri_params = {
+        'project_id': project_id, 'execution_name': execution_name, 'execution_from': execution_from,
+        'execution_type': execution_type
+    }
+    response = requests.get(generate_report_uri, params=uri_params, verify=ssl_verification)
+    log_response(response, with_report_url=True)
+
+
+def main():
+    send_results_to_allure_server()
+    generate_allure_report()
+
+
+if __name__ == '__main__':
+    main()
--- allure-docker-api-usage/send_results_security.py
+++ allure-docker-api-usage/send_results_security.py
@@ -1,4 +1,5 @@
 import os, requests, json, base64
+from requests import Response
 
 # This directory is where you have all your results locally, generally named as `allure-results`
 allure_results_directory = '/allure-results-example'
@@ -6,92 +7,111 @@ allure_results_directory = '/allure-results-example'
 allure_server = 'http://localhost:5050'
 # Project ID according to existent projects in your Allure container - Check endpoint for project creation >> `[POST]/projects`
 project_id = 'default'
-#project_id = 'my-project-id'
+# project_id = 'my-project-id'
+
 # Set security_user & security_password according to Allure container configuration
-security_user='my_username'
-security_password='my_password'
+security_user = 'my_username'
+security_password = 'my_password'
+
+# Connection configuration
+send_results_uri = f'{allure_server}/allure-docker-service/send-results'
+generate_report_uri = f'{allure_server}/allure-docker-service/generate-report'
+login_uri = f'{allure_server}/allure-docker-service/login'
+
+ssl_verification = True
 
 current_directory = os.path.dirname(os.path.realpath(__file__))
 results_directory = current_directory + allure_results_directory
 print('RESULTS DIRECTORY PATH: ' + results_directory)
 
-files = os.listdir(results_directory)
 
-print('FILES:')
-results = []
-for file in files:
+def get_file_as_result(filename):
+    file_path = results_directory + "/" + filename
     result = {}
 
-    file_path = results_directory + "/" + file
-    print(file_path)
-
     if os.path.isfile(file_path):
-        try:
-            with open(file_path, "rb") as f:
-                content = f.read()
-                if content.strip():
-                    b64_content = base64.b64encode(content)
-                    result['file_name'] = file
-                    result['content_base64'] = b64_content.decode('UTF-8')
-                    results.append(result)
-                else:
-                    print('Empty File skipped: '+ file_path)
-        finally :
-            f.close()
+        with open(file_path, "rb") as f:
+            content = f.read()
+            if content.strip():
+                b64_content = base64.b64encode(content)
+                result['file_name'] = filename
+                result['content_base64'] = b64_content.decode('UTF-8')
+                print(f'Successfully encoded result for {file_path}')
+                return result
+            else:
+                print('Empty File skipped: ' + file_path)
     else:
-        print('Directory skipped: '+ file_path)
-
-headers = {'Content-type': 'application/json'}
-request_body = {
-    "results": results
-}
-json_request_body = json.dumps(request_body)
-
-ssl_verification = True
-
-print("------------------LOGIN-----------------")
-credentials_body = {
-    "username": security_user,
-    "password": security_password
-}
-json_credentials_body = json.dumps(credentials_body)
-
-session = requests.Session()
-response = session.post(allure_server + '/allure-docker-service/login', headers=headers, data=json_credentials_body, verify=ssl_verification)
-
-print("STATUS CODE:")
-print(response.status_code)
-print("RESPONSE COOKIES:")
-json_prettier_response_body = json.dumps(session.cookies.get_dict(), indent=4, sort_keys=True)
-print(json_prettier_response_body)
-csrf_access_token = session.cookies['csrf_access_token']
-print("CSRF-ACCESS-TOKEN: " + csrf_access_token)
+        print('Directory skipped: ' + file_path)
 
 
-print("------------------SEND-RESULTS------------------")
-headers['X-CSRF-TOKEN'] = csrf_access_token
-response = session.post(allure_server + '/allure-docker-service/send-results?project_id=' + project_id, headers=headers, data=json_request_body, verify=ssl_verification)
-print("STATUS CODE:")
-print(response.status_code)
-print("RESPONSE:")
-json_response_body = json.loads(response.content)
-json_prettier_response_body = json.dumps(json_response_body, indent=4, sort_keys=True)
-print(json_prettier_response_body)
+def gather_result_files():
+    files = os.listdir(results_directory)
+    print('FILES:')
+    results = list(filter(lambda f: f is not None, [get_file_as_result(file) for file in files]))
+    return results
 
-# If you want to generate reports on demand use the endpoint `GET /generate-report` and disable the Automatic Execution >> `CHECK_RESULTS_EVERY_SECONDS: NONE`
-"""
-print("------------------GENERATE-REPORT------------------")
-execution_name = 'execution from my script'
-execution_from = 'http://google.com'
-execution_type = 'teamcity'
-response = session.get (allure_server + '/allure-docker-service/generate-report?project_id=' + project_id + '&execution_name=' + execution_name + '&execution_from=' + execution_from + '&execution_type=' + execution_type, headers=headers, verify=ssl_verification)
-print("STATUS CODE:")
-print(response.status_code)
-print("RESPONSE:")
-json_response_body = json.loads(response.content)
-json_prettier_response_body = json.dumps(json_response_body, indent=4, sort_keys=True)
-print(json_prettier_response_body)
 
-print('ALLURE REPORT URL:')
-print(json_response_body['data']['report_url'])
-"""
+def log_response(response: Response, with_report_url=False):
+    print("STATUS CODE:")
+    print(response.status_code)
+    print("RESPONSE:")
+    json_prettier_response_body = json.dumps(response.json(), indent=4, sort_keys=True)
+    print(json_prettier_response_body)
+    if with_report_url:
+        print('ALLURE REPORT URL:')
+        print(response.json()['data']['report_url'])
+
+
+def get_authenticated_session():
+    print("------------------LOGIN-----------------")
+    session = requests.Session()
+    credentials_body = {
+        "username": security_user,
+        "password": security_password
+    }
+    response = session.post(login_uri, json=credentials_body, verify=ssl_verification)
+    log_response(response)
+
+    csrf_access_token = session.cookies['csrf_access_token']
+    print("CSRF-ACCESS-TOKEN: " + csrf_access_token)
+    session.headers['X-CSRF-TOKEN'] = csrf_access_token
+    return session
+
+
+def send_results_to_allure_server(session):
+    print("------------------SEND-RESULTS------------------")
+    request_body = {
+        "results": gather_result_files()
+    }
+    uri_params = {
+        'project_id': project_id
+    }
+    response = session.post(send_results_uri, params=uri_params, json=request_body, verify=ssl_verification)
+    log_response(response)
+
+
+def generate_allure_report(session):
+    """
+    If you want to generate reports on demand use the endpoint `GET /generate-report` and disable the Automatic Execution >> `CHECK_RESULTS_EVERY_SECONDS: NONE`
+    """
+    print("------------------GENERATE-REPORT------------------")
+    execution_name = 'execution from my script'
+    execution_from = 'http://google.com'
+    execution_type = 'teamcity'
+
+    uri_params = {
+        'project_id': project_id, 'execution_name': execution_name, 'execution_from': execution_from,
+        'execution_type': execution_type
+    }
+    response = session.get(generate_report_uri, params=uri_params, verify=ssl_verification)
+    log_response(response, with_report_url=True)
+
+
+def main():
+    session = get_authenticated_session()
+    send_results_to_allure_server(session)
+    generate_allure_report(session)
+
+
+if __name__ == '__main__':
+    main()
